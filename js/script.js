2e3bb037a390faffbbf4f8ea36a8bf74507aae6c
skonns <109499764+skonns@users.noreply.github.com>
2023-12-05 20:15:17 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function showDescription(element) {
+    element.querySelector('.deskripsi').style.opacity = '1';
+    element.style.transform = 'scale(1.1)';
+  }
+  
+  function hideDescription(element) {
+    element.querySelector('.deskripsi').style.opacity = '0';
+    element.style.transform = 'scale(1)';
+  }
+  
